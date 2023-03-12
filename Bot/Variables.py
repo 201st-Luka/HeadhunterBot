4f7d13ccda5f0ea895a787606a304312d0fd5ee5
201st-Luka <luka.eichten@gmail.com>
2023-03-12 15:42:17 +0100

    tokens

--- Bot/Variables.py
+++ Bot/Variables.py
@@ -1,9 +1,9 @@
 import os
 
-discordApiToken = "NjMwMzMyNDAzODY0NTY3ODA4.GHl_9-.CBuEZoqP0_sz6xST-euA1VJVFNkzt3ZqXgYSL4"
+discordApiToken = "your api token"
 clashOfClansHeaders = {
     "Accept": "application/json",
-    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjEwNjQyN2E0LWIzNmUtNGFkNi1hZTYwLWQzOTUyNDY3N2I0MiIsImlhdCI6MTY2NzgzMDA5Miwic3ViIjoiZGV2ZWxvcGVyL2VhOWNkOWYwLTQwMDMtZjI1My05MmZkLWI1OTE3ZmNiM2FkOCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEzOS4xNjUuMzEuMTQiLCIxMzkuMTY1LjMxLjE1IiwiODEuMjQ0LjIxOC4xNTEiLCIxMDkuMTMwLjczLjE2OCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.r8_38CAKrLSMDU3o0tCvuiu-YS7EzpIwMv6qNyDnrf_PhaC1qL0rknHhiSjklBAYtxfCA2e5oZ0rho74PezHNg"
+    "authorization": "Bearer your coc header"
 }
 messageOnGuildJoin = "Hello there!"
 discordServer = "no discord server yet"
