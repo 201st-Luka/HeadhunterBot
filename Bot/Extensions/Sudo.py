3476e17520a67b3bb551cb7af3cefc6a812dbcff
201st-Luka <luka.eichten@gmail.com>
2023-08-05 13:18:19 +0200

    implemented the `sudo blacklist` command

--- Bot/Extensions/Sudo.py
+++ Bot/Extensions/Sudo.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 
 from interactions import slash_command, SlashCommandOption, OptionType, Extension, Client, SlashContext, Permissions, \
-    SlashCommandChoice, User, SlashCommand, AutoDefer, ChannelType, GuildChannel
+    SlashCommandChoice, User, SlashCommand, AutoDefer, ChannelType, GuildChannel, Embed, Color, Modal, ParagraphText, \
+    ModalContext
+from interactions.ext.paginators import Paginator
 from pyclasher import PlayerRequest, ClanRequest
 from pyclasher.models import ApiCodes
 
@@ -16,6 +18,7 @@ class SudoCommand(Extension):
 
     def __init__(self, client: HeadhunterClient):
         self.client = client
+        return
 
     sudo = SlashCommand(
         name="sudo",
@@ -108,8 +111,94 @@ class SudoCommand(Extension):
         sub_cmd_name="info",
         sub_cmd_description="player blacklist of players that are not welcome in the clan",
     )
-    async def blacklist(self, ctx: SlashContext) -> None:
-        await ctx.send("not implemented yet")
+    async def info(self, ctx: SlashContext) -> None:
+        black_list = self.client.db_user.guild_blacklist.fetch_players(ctx.guild_id)
+
+        if not len(black_list):
+            await ctx.send("The blacklist for this guild is empty.")
+            return
+
+        embeds: list[Embed] = []
+
+        for start in range(0, len(black_list), self.client.cfg['players_per_page']):
+            embed = Embed(
+                title=f"Blacklist for {ctx.guild.name}",
+                description=f"{ctx.guild.name} has {len(black_list)} account"
+                            f"{'' if len(black_list) == 1 else 's'} on the blacklist",
+                color=Color.from_hex(self.client.cfg['embed_color'].strip('0x') if
+                                     self.client.cfg['embed_color'].startswith('0x') else
+                                     self.client.cfg['embed_color'])
+            )
+
+            for player in black_list[start:start + self.client.cfg['players_per_page']]:
+                embed.add_field(
+                    name=f"__{player[1]} ({player[0]})__",
+                    value=f"__Reason__: {player[2] if player[2] is not None else 'not provided'}"
+                )
+
+            embeds.append(embed)
+
+        if len(embeds) == 1:
+            await ctx.send(embeds=embeds)
+            return
+
+        paginator = Paginator.create_from_embeds(self.client, *embeds)
+        paginator.default_color = Color.from_hex(self.client.cfg['embed_color'].strip('0x') if
+                                                 self.client.cfg['embed_color'].startswith('0x') else
+                                                 self.client.cfg['embed_color'])
+        paginator.show_select_menu = True
+
+        await paginator.send(ctx)
+        return
+
+    @blacklist.subcommand(
+        sub_cmd_name="add",
+        sub_cmd_description="add a player to the guild blacklist",
+        options=[
+            PlayerOption
+        ]
+    )
+    async def add(self, ctx: SlashContext, player: Annotated[PlayerRequest, PlayerTagConverter]) -> None:
+        blacklist = self.client.db_user.guild_blacklist.fetch_players(ctx.guild_id)
+
+        if not len(blacklist) or player.tag not in [row[0] for row in blacklist]:
+            modal = Modal(
+                ParagraphText(
+                    label=f"Blacklisting {player.name} reason",
+                    custom_id=f"{ctx.guild.id}_{player.tag}_modal_sudo_blacklist_add_label",
+                    placeholder="Enter your reason for a ban or leave this field empty",
+                    max_length=1500
+                ),
+                title="Reason"
+            )
+
+            await ctx.send_modal(modal)
+
+            modal_ctx: ModalContext = await self.client.wait_for_modal(modal)
+
+            reason = "N/A"
+            if (response := modal_ctx.responses[f'{ctx.guild.id}_{player.tag}_modal_sudo_blacklist_add_label']) != "":
+                reason = response
+
+            self.client.db_user.guild_blacklist.insert_player(ctx.guild_id, player.tag, player.name,
+                                                              response if response != "" else None)
+
+            await modal_ctx.send(content=f"Added **{player.name}** {player.tag} to the blacklist. Reason:\n*"
+                                         f"{reason}*")
+            return
+
+        else:
+            reason = 'N/A'
+            for sublist in blacklist:
+                if sublist[0] == player.tag and sublist[2] is not None:
+                    reason = sublist[2]
+            await ctx.send(f"The player {player.name} {player.tag} is already on the blacklist. Reason:\n*{reason}*")
+            return
+
+    @add.error
+    async def on_add_error(self, exception: Exception, ctx: SlashContext, player: str = None) -> None:
+        if isinstance(exception, InvalidPlayerTag):
+            await ctx.send(str(InvalidPlayerTag(player)), ephemeral=True)
         return
 
     @slash_command(
